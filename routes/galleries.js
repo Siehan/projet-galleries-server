13268d177e60e6ae90230db9695f3461c243baad
Siehan <memain.ye.sylvie@gmail.com>
2020-03-04 18:06:21 +0100

    modifs newGallery

--- routes/galleries.js
+++ routes/galleries.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const router = new express.Router();
 const galleryModel = require("../models/Gallery");
+const imageModel = require("../models/Image");
+
 const uploader = require("../config/cloudinary")
 
 router.post("/galleries", uploader.single("background"), (req, res) => {
-    console.log(req.body, "toto")
-    galleryModel
-        .create(req.body)
-        .then(createdGallery => {
-            res.status(200).json({
-                createdGallery
-            })
+
+    let images = JSON.parse(req.body.images)[0]; // array format
+
+    const copy = images.map(function (element) {
+        return { url: element }
+    })
+    
+    imageModel.insertMany(copy)
+        .then(addedImages => {
+            const newGallery = req.body
+            newGallery.images = addedImages
+
+            galleryModel
+                .create(newGallery)
+                .then(createdGallery => {
+                    res.status(200).json({
+                        createdGallery
+                    })
+                })
+                .catch(err => res.status(500).json(err))
         })
-        .catch(err => res.status(500).json(err))
+        .catch(err=> console.log(err))
+
+
 });
 
 router.get("/galleries", async (req, res, next) => {
